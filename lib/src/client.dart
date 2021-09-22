234c8d29d3dde20f582249816fca27936744ce5c
kevinboss <ttson.hcmus@gmail.com>
2021-09-22 15:52:20 +0700

    Added im.recent.list [Missing]

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,25 +1,36 @@
 import 'package:bitrixmobile_client/bitrixmobile_client.dart';
-import 'package:bitrixmobile_client/src/auth.dart';
+import 'package:bitrixmobile_client/src/auth/auth_client.dart';
 import 'package:bitrixmobile_client/src/http/http_client.dart';
 import 'package:bitrixmobile_client/src/http/http_client_impl.dart';
 
+import 'chat/chat_client.dart';
+
 class BitrixClient {
   late final String baseUrl;
-  late final ApiConfigurations apiConfigurations;
-  late final HttpClient httpClient;
+  final ApiConfigurations apiConfigurations;
+  final HttpClient httpClient;
 
   // features client
-  late final AuthManagement authManagement;
+  late final AuthClient authClient;
+  late final ChatClient chatClient;
 
-  BitrixClient(this.baseUrl, this.apiConfigurations,
+  BitrixClient(String baseUrl, this.apiConfigurations,
       {Map<String, String> headers = const {},
-      Map<String, String> cookies = const {}}) {
-    httpClient = HttpClientImpl(defaultHeaders: headers, cookies: cookies);
+      Map<String, String> cookies = const {}})
+      : httpClient = HttpClientImpl(defaultHeaders: headers, cookies: cookies) {
+
+    if (!baseUrl.endsWith('/')) {
+      this.baseUrl = baseUrl;
+    } else {
+      this.baseUrl = baseUrl + '/';
+    }
+
     final hostRegex = RegExp(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]*');
     final validhost = hostRegex.hasMatch(baseUrl);
 
     if (!validhost) throw Exception('Uri is wrong format.');
 
-    authManagement = AuthManagement(this);
+    authClient = AuthClient(this);
+    chatClient = ChatClientImpl(this);
   }
 }
